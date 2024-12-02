fb99803faea63b97750213a5e1718183c30f1971
laurencosgrove <laurenacosgrove@gmail.com>
2024-12-02 13:01:39 +0000

    done part 2

--- day-2/2.py
+++ day-2/2.py
@@ -9,9 +9,8 @@ def main():
 
     safe = 0
 
-    for i in reports.keys():
-        if is_safe(reports[i]):
-            safe += 1
+    print(one_level_away(reports))
+
 
     print(safe)
 
@@ -37,14 +36,14 @@ def is_safe(report: list):
     if report[0] < report[1]:
          increasing = True
          jump = abs(report[1] - report[0])
-         if jump > 3 or jump < 1:
+         if jump > 3:
             return False
     elif report[0] == report[1]:
         return False # immediately return false here because this means list isn't increasing or decreasing
     else:
         increasing = False
         jump = abs(report[1] - report[0])
-        if jump > 3 or jump < 1:
+        if jump > 3:
             return False
 
     if increasing:
@@ -52,18 +51,45 @@ def is_safe(report: list):
             jump = abs(report[i] - report[i+1])
             if report[i] > report[i+1] or report[i] == report[i+1]:
                 return False
-            elif jump > 3 or jump < 1:
+            elif jump > 3:
                 return False
     else: # decreasing
         for i in range(1, len(report)-1): # start from by comparing 1 -> 2, we already checked 0 -> 1
             jump = abs(report[i] - report[i+1])
             if report[i] < report[i+1] or report[i] == report[i+1]:
                 return False
-            elif jump > 3 or jump < 1:
+            elif jump > 3:
                 return False
             
     return True
 
+def one_level_away(reports: dict):
+    # get the unsafe reports:
+    unsafe_reports = []
+    safe = 0
 
+    for i in reports.keys():
+        if not is_safe(reports[i]):
+            unsafe_reports.append(i)
+        else:
+            safe +=1
+
+    for i in unsafe_reports:
+        if check_unsafe_report(reports[i]):
+            safe += 1
+
+    return safe
+                
+def check_unsafe_report(report: list):
+    for i in range(len(report)): 
+        removed_item = report[i]  # save the element to remove
+        del report[i]  
+        if is_safe(report):
+            return True
+        report.insert(i, removed_item)  # restore the removed element
+    return False 
+
+
+     
 if __name__ == "__main__":
     main()
